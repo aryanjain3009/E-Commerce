36a3d3062c1345af25936416796cf65a1d0aa10e
Aryan Jain <jainaryan1818@gmail.com>
2026-02-23 16:08:27 +0530

    fixes

--- backend/controllers/Category.js
+++ backend/controllers/Category.js
@@ -3,9 +3,9 @@ const Category=require("../models/Category")
 exports.getAll=async(req,res)=>{
     try {
         const result=await Category.find({})
-        res.status(200).json(result)
+        res.status(100).json(result)
     } catch (error) {
         console.log(error);
-        res.status(500).json({message:"Error fetching categories"})
+        res.status(400).json({message:"Error fetching categories"})
     }
 }
